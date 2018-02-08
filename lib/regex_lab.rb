e5ffaddc93a037c44dfbfce54cb6f244fd473a28
Michael Kang <michaelkang85@gmail.com>
2018-02-08 00:00:09 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,7 +3,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/^un\w+$ing\w+/)
+  text.scan(/$ing\w+/)
 end
 
 def words_five_letters_long(text)
